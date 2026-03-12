0a685fbb5ec1cb6d86313e8f018925ff353c2092
JityaBusi <119723522+JityaBusi@users.noreply.github.com>
2026-03-12 19:34:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use(express.json());
 
 app.use("/api/media", mediaRoutes);
 
-// app.listen(process.env.PORT, () => {
-//   console.log(`Server running on port ${process.env.PORT}`);
-// });
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
 
 // Export the app for Vercel serverless functions
-module.exports = app;
+// module.exports = app;
