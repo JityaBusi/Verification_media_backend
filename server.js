4c1d4a0556b1a367e34c2c48306cd68ae4dec737
JityaBusi <119723522+JityaBusi@users.noreply.github.com>
2026-03-13 18:31:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ app.use(express.json());
 
 app.use("/api/media", mediaRoutes);
 
+app.get('/',(req,res)=>{
+  res.send({
+    activeStatus:true,
+    error:false,
+  })
+})
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
